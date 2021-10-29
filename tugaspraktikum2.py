55e07bfbf10c1bfbff3fb39e30f20290f515aa88
RifkyAH <rifkyabdillah223@gmail.com>
2021-10-29 21:37:55 +0700

    tugas praktikum 2

---
+++ tugaspraktikum2.py
@@ -0,0 +1,12 @@
+bil_pertama=input("Masukkan bilangan ke1:")
+bil_kedua=input("Masukkan bilangan ke2:")
+bil_ketiga=input("Masukkan bilangan ke3:")
+
+if bil_pertama>bil_kedua>bil_ketiga:
+           print(bil_pertama,"adalah bilangan terbesar")
+elif bil_pertama<bil_kedua>bil_ketiga:
+    print(bil_kedua,"adalah bilangan terbesar")
+else:
+    print(bil_ketiga,"adalah bilangan terbesar")
+        
+           
